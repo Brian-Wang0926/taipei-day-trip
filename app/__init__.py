845c080fae3887709f1b240a84727ec1ae479a8b
Brian-Wang0926 <aa0988402763@gmail.com>
2024-01-04 21:26:54 +0800

    更新資料庫

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_bcrypt import Bcrypt
 load_dotenv()
 db_username = os.getenv('DB_USERNAME')
 db_password = os.getenv('DB_PASSWORD')
-
+db_host = os.getenv('DB_HOST')  # RDS endpoint
+db_name = os.getenv('DB_NAME')  # Database name
 
 db = SQLAlchemy()
 
@@ -23,7 +24,7 @@ def create_app():
     app.config['MERCHANT_ID'] = os.getenv('MERCHANT_ID')
 
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_username}:{db_password}@localhost/taipei_trip_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_POOL_SIZE'] = 20  # 設定連接池大小
     app.config['SQLALCHEMY_POOL_TIMEOUT'] = 10  # 設定連接超時時間
